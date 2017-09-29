ae6d8600a5dcf9f4c44dac4cdbdbce23519d3f5a
minidonut <nycom13@gmail.com>
2017-09-29 23:32:17 +0000

    grid-layout

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
-/*global Vue*/
-
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: '안녕하세요 Vue!',
+/*
+  
     company: [
         { name: "CSH" , location: "HOME" },
         { name: "MKH" , location: "OUTSIDE" },
         { name: "JSW" , location: "OUTSIDE" }
         ]
-  },
-  methods: {
-      greet: function(event){
-          alert("Hello world!");
-      }
-  }
-})
-
-
-var Man = function(name, grade, location, action){
-    
-    this.location = location;
-    this.action = action;
-    this.grade = grade;
-    this.name = name;
-    
-}
-
-
+  */
